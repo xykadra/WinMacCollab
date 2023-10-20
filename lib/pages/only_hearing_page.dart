b449278c0bebc0de6de4497579422442fbc30407
Mirza Kadrić <kadric681@gmail.com>
2023-10-20 18:37:14 +0200

    Version5

--- lib/pages/only_hearing_page.dart
+++ lib/pages/only_hearing_page.dart
@@ -11,55 +11,57 @@ class OnlyHearingPage extends StatefulWidget {
 class _OnlyHearingPageState extends State<OnlyHearingPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Container(
-        color: Colors.white,
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                    child: Container(
-                      padding: EdgeInsets.all(5),
-                      decoration: BoxDecoration(
-                          color: Colors.black,
-                          borderRadius: BorderRadius.circular(20)),
-                      child: Icon(
-                        Icons.arrow_back_ios_rounded,
-                        color: Colors.white,
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Container(
+          color: Colors.white,
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: Container(
+                        padding: EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                            color: Colors.black,
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Icon(
+                          Icons.arrow_back_ios_rounded,
+                          color: Colors.white,
+                        ),
                       ),
                     ),
-                  ),
-                  Expanded(
-                    child: LinearPercentIndicator(
-                      lineHeight: 30,
-                      barRadius: Radius.circular(12),
-                      percent: 0.4,
-                      progressColor: Color.fromRGBO(245, 193, 68, 1),
-                      backgroundColor:
-                          Color.fromRGBO(245, 193, 68, 1).withOpacity(0.4),
+                    Expanded(
+                      child: LinearPercentIndicator(
+                        lineHeight: 30,
+                        barRadius: Radius.circular(12),
+                        percent: 0.4,
+                        progressColor: Color.fromRGBO(245, 193, 68, 1),
+                        backgroundColor:
+                            Color.fromRGBO(245, 193, 68, 1).withOpacity(0.4),
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              Spacer(),
-              Container(
-                height: MediaQuery.of(context).size.height / 2,
-                decoration: BoxDecoration(
-                  color: Colors.red,
-                  borderRadius: BorderRadius.circular(12),
+                  ],
                 ),
-              ),
-              Spacer(),
-            ],
+                Spacer(),
+                Container(
+                  height: MediaQuery.of(context).size.height / 2,
+                  decoration: BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+                Spacer(),
+              ],
+            ),
           ),
         ),
       ),
